6c2978692d2fc57e06f92dcf32e43b8405563d00
tony.hillerson <tony.hillerson@47e52f69-1a3c-0410-a923-c93d89df8d96>
2008-06-16 04:06:26 +0000

    checking in fixes for rails 2.1 - amf mime types are now ignored from forgery protection checks in rails
    git-svn-id: http://rubyamf.googlecode.com/svn/trunk/rubyamf@1283 47e52f69-1a3c-0410-a923-c93d89df8d96

---
+++ app/mime_type.rb
@@ -0,0 +1,3 @@
+# Adding amf to the list of mime type formats that are not checked for forgery protection
+# at the time of writing (rails 2.1) the other types were [:text, :json, :csv, :xml, :rss, :atom, :yaml]
+Mime::Type.unverifiable_types << :amf
--- init.rb
+++ init.rb
@@ -5,6 +5,7 @@ require 'util/string'
 require 'util/vo_helper'
 require 'util/active_record'
 require 'util/action_controller'
+require 'app/mime_type'
 require 'app/fault_object'
 require 'app/rails_gateway'
 require File.expand_path(RAILS_ROOT) + '/config/rubyamf_config' #run the configuration
