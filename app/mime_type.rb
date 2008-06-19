ecd96a9d07757cff88d347cd2a415ca2ba811d90
tony.hillerson <tony.hillerson@47e52f69-1a3c-0410-a923-c93d89df8d96>
2008-06-19 01:54:31 +0000

    fixed backwards compatibility issue with 2.1 fixes
    git-svn-id: http://rubyamf.googlecode.com/svn/trunk/rubyamf@1288 47e52f69-1a3c-0410-a923-c93d89df8d96

--- app/mime_type.rb
+++ app/mime_type.rb
@@ -1,3 +1,3 @@
 # Adding amf to the list of mime type formats that are not checked for forgery protection
 # at the time of writing (rails 2.1) the other types were [:text, :json, :csv, :xml, :rss, :atom, :yaml]
-Mime::Type.unverifiable_types << :amf
+Mime::Type.unverifiable_types << :amf if defined? Mime::Type.unverifiable_types
